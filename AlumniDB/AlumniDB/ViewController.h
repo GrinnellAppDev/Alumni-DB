3530490505648379fb6b9b7500ba092c983e7ee6
Lea Marolt <maroltso@grinnell.edu>
2014-09-16 17:16:34 -0500

    Setting up the project

---
+++ AlumniDB/AlumniDB/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AlumniDB
+//
+//  Created by Lea Marolt on 9/16/14.
+//  Copyright (c) 2014 Grinnell AppDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
